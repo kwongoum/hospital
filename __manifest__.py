9539341fa884a32e03ec27104eb7d8146478c7fd
Avit Wongoum <avit.wongoum@convivial.be>
2026-04-17 21:19:34 +0200

    first commit :hospital module in odoo

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Hospital Management",
+    "version": "1.0",
+    "summary": "Hospital custom Modules",
+    "author": "Avit Merlin W. Kongoum",
+    "sequence": -100,
+    "category": "Custom",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
